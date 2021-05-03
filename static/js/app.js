66703049ded72e47a9de6b0b0d3f9637e1032d97
Richard Moseley <rwmoseley1219@gmail.com>
2021-05-02 19:23:59 -0500

    finalized

--- static/js/app.js
+++ static/js/app.js
@@ -18,25 +18,39 @@ function buildPlots(userInput) {
         var top_ten_ids = filtered_ids.slice(0, 10).reverse()
         var top_ten_samples = filtered_sample_values.slice(0, 10).reverse()
         var top_ten_labels = filtered_labels.slice(0, 10).reverse()
-        var trace1 =
-        {
-            y: top_ten_ids.map(i => `OTU ${i}`),
+        var trace1 = {
             x: top_ten_samples,
+            y: top_ten_ids.map(i => `OTU ${i}`),
             text: top_ten_labels,
             type: "bar",
             orientation: "h",
         }
         Plotly.newPlot('bar', [trace1])
+        var trace2 = {
+            x: filtered_ids,
+            y: filtered_sample_values,
+            text: filtered_labels,
+            mode: 'markers',
+            marker: {
+                size: filtered_sample_values,
+                color: filtered_ids
+            }
+        }
+        Plotly.newPlot('bubble', [trace2])
+        var demo_card_li = d3.select('#sample-metadata')
+        var selected_data = d.metadata.filter(i => i.id == userInput)
+        selected_data.forEach(i => {
+            demo_card_li.append('li').html(`ID: ${i.id}<br>`)
+            demo_card_li.append('li').html(`Ethnicity: ${i.ethnicity}<br>`)
+            demo_card_li.append('li').html(`Gender: ${i.gender}<br>`)
+            demo_card_li.append('li').html(`Age: ${i.age}<br>`)
+            demo_card_li.append('li').html(`Location: ${i.location}<br>`)
+            demo_card_li.append('li').html(`Belly Button Type: ${i.bbtype}<br>`)
+            demo_card_li.append('li').html(`Wash Frequency: ${i.wfreq}<br>`)
+        })
     })
 }
 
 function optionChanged(option) {
     buildPlots(option)
 }
-// Use sample_values as the values for the bar chart.
-
-
-// Use otu_ids as the labels for the bar chart.
-
-
-// Use otu_labels as the hovertext for the chart.
